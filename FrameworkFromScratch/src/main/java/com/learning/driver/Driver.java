cc8258f347fd85e9b5b5298d9e562028d5f8f318
Pavithra Sathyanarayanan <75259736+pavithrasathya2098@users.noreply.github.com>
2023-03-18 13:02:19 +0530

    Masked password and added decodeUtil

--- FrameworkFromScratch/src/main/java/com/learning/driver/Driver.java
+++ FrameworkFromScratch/src/main/java/com/learning/driver/Driver.java
@@ -3,8 +3,13 @@ package com.learning.driver;
 import com.learning.enums.ConfigProperties;
 import com.learning.util.PropertiesUtil;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.Objects;
 
 public final class Driver {
@@ -17,6 +22,9 @@ public final class Driver {
             if (browser.equalsIgnoreCase("chrome")) {
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
+//                DesiredCapabilities dp = new DesiredCapabilities();
+//                dp.setBrowserName("Chrome");
+                
                 DriverManager.setDriverThreadLocal(WebDriverManager.chromedriver().capabilities(options).create());
             } else {
                 DriverManager.setDriverThreadLocal(WebDriverManager.safaridriver().create());
