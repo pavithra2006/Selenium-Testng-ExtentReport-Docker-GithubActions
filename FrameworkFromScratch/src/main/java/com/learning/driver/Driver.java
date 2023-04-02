b0b134e978e4b3a98f824cc31164f7c5c4fe007c
pavithra2006 <pavi2098@gmail.com>
2023-04-02 07:27:42 +0530

    Removed unwanted comments

--- FrameworkFromScratch/src/main/java/com/learning/driver/Driver.java
+++ FrameworkFromScratch/src/main/java/com/learning/driver/Driver.java
@@ -13,7 +13,6 @@ import java.util.Objects;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Driver {
 //    private Driver() {
-//
 //    }
 
     public static void init(String browser) {
