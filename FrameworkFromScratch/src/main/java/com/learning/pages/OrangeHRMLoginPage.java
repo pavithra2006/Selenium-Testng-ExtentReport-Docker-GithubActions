42fd319b0ed15538d59c1293595971a373a9d158
pavithra2006 <pavi2098@gmail.com>
2023-04-18 17:19:48 +0530

    Removed unwanted logs

--- FrameworkFromScratch/src/main/java/com/learning/pages/OrangeHRMLoginPage.java
+++ FrameworkFromScratch/src/main/java/com/learning/pages/OrangeHRMLoginPage.java
@@ -24,7 +24,6 @@ public final class OrangeHRMLoginPage extends BasePage {
     }
 
     public String getTitle() {
-        System.out.println(getPageTitle());
         return getPageTitle();
     }
 }
